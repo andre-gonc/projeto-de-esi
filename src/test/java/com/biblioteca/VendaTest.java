e445e3c13fae1d3b5ea72d099c15f463c215b401
AndreAraujo1998 <andre.florencio98@gmail.com>
2023-09-22 19:12:22 -0300

    .

--- src/test/java/com/biblioteca/VendaTest.java
+++ src/test/java/com/biblioteca/VendaTest.java
@@ -17,14 +17,14 @@ public class VendaTest {
                 Editora editora = new Editora("Minha Editora");
                 Livro livro = new Livro(autor, editora, "1234567890", "O Soberano", "633");
                 LivroEstoque livroDoEstoque = new LivroEstoque(livro, 10, 100.00);
-                LivroVenda livroVenda = new LivroVenda(livroDoEstoque, 3);
+                LivroVenda livroVenda = new LivroVenda(livroDoEstoque,3);
                 
                 Venda venda = new Venda("João", "Crédito", 100.00, livroVenda);
                 
                 Assert.assertEquals("João", venda.nomeCliente);
                 Assert.assertEquals("Crédito", venda.formaPagamento);
                 Assert.assertEquals(100.00, venda.valorDado, 0.001);
-                Assert.assertEquals(livroVenda, venda.livroVenda)
+                Assert.assertEquals(livroVenda, venda.livroVenda);
         }
 
         /**
