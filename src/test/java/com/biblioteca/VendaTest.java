88301342d92d3a84c22bea6dded98c2f4951d2a9
AndreAraujo1998 <andre.florencio98@gmail.com>
2023-09-22 19:22:24 -0300

    .

--- src/test/java/com/biblioteca/VendaTest.java
+++ src/test/java/com/biblioteca/VendaTest.java
@@ -1,5 +1,5 @@
 package com.biblioteca;
-
+import java.util.ArrayList;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,10 +15,11 @@ public class VendaTest {
 
                 Autor autor = new Autor("João", "Gonçalves");
                 Editora editora = new Editora("Minha Editora");
-                Livro livro = new Livro(autor, editora, "1234567890", "O Soberano", "633");
+                Livro livro = new Livro(autor, editora, "1234567890", "O Soberano", 633);
                 LivroEstoque livroDoEstoque = new LivroEstoque(livro, 10, 100.00);
-                LivroVenda livroVenda = new LivroVenda(livroDoEstoque,3);
-                
+                nossoLivro = new ArrayList<LivroEstoque>();
+                nossoLivro.add(livroDoEstoque);
+                LivroVenda livroVenda = new LivroVenda(nossoLivro, 3);
                 Venda venda = new Venda("João", "Crédito", 100.00, livroVenda);
                 
                 Assert.assertEquals("João", venda.nomeCliente);
