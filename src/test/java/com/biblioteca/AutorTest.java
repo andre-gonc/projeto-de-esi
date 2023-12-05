2905aff6cf0b704dbb875d6c081fba20d993bd48
andre-gonc <142347776+andre-gonc@users.noreply.github.com>
2023-12-05 13:40:03 -0300

    Update AutorTest.java
    Atualização na classe AutorTest.java para alterar a versão do JUnit utilizada de JUnit Jupiter para JUnit4

--- src/test/java/com/biblioteca/AutorTest.java
+++ src/test/java/com/biblioteca/AutorTest.java
@@ -1,5 +1,5 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class AutorTest {
 
