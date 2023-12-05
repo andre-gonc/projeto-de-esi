7d51d984e0bfae2fdac652371f7bc76256cc2a36
andre-gonc <142347776+andre-gonc@users.noreply.github.com>
2023-12-05 20:03:01 -0300

    Update AutorTest.java
    Atualização no uso de todos os assertEquals, adicionando o "Assert." para acioná-las corretamente

--- src/test/java/com/biblioteca/AutorTest.java
+++ src/test/java/com/biblioteca/AutorTest.java
@@ -11,40 +11,40 @@ public class AutorTest {
     @Test
     public void testConstrutorPadrao() {
         Autor autor = new Autor();
-        assertEquals(null, autor.nome);
-        assertEquals(null, autor.sobrenome);
+        Assert.assertEquals(null, autor.nome);
+        Assert.assertEquals(null, autor.sobrenome);
     }
 
     @Test
     public void testConstrutorComParametros() {
         Autor autor = new Autor("John", "Doe");
-        assertEquals("John", autor.nome);
-        assertEquals("Doe", autor.sobrenome);
+        Assert.assertEquals("John", autor.nome);
+        Assert.assertEquals("Doe", autor.sobrenome);
     }
 
     @Test
     public void testGetNome() {
         Autor autor = new Autor("Jane", "Doe");
-        assertEquals("Jane", autor.nome);
+        Assert.assertEquals("Jane", autor.nome);
     }
 
     @Test
     public void testGetSobrenome() {
         Autor autor = new Autor("Jane", "Doe");
-        assertEquals("Doe", autor.sobrenome);
+        Assert.assertEquals("Doe", autor.sobrenome);
     }
 
     @Test
     public void testSetNome() {
         Autor autor = new Autor();
         autor.nome = "Alice";
-        assertEquals("Alice", autor.nome);
+        Assert.assertEquals("Alice", autor.nome);
     }
 
     @Test
     public void testSetSobrenome() {
         Autor autor = new Autor();
         autor.sobrenome = "Smith";
-        assertEquals("Smith", autor.sobrenome);
+        Assert.assertEquals("Smith", autor.sobrenome);
     }
 }
